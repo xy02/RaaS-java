9140e774d9df3dfaa8f1bbe10f4e45a09a1fec9b
xy02 <xyxy0202@qq.com>
2018-06-19 11:36:01 +0800

    subWithQueue

--- src/main/java/com/github/xy02/raas/nats/NatsNode.java
+++ src/main/java/com/github/xy02/raas/nats/NatsNode.java
@@ -1,7 +1,8 @@
 package com.github.xy02.raas.nats;
 
-import com.github.xy02.nats.Client;
-import com.github.xy02.nats.Msg;
+import com.github.xy02.nats.Connection;
+import com.github.xy02.nats.MSG;
+import com.github.xy02.nats.Options;
 import com.github.xy02.raas.DataOuterClass.Data;
 import com.github.xy02.raas.RaaSNode;
 import com.github.xy02.raas.Service;
@@ -18,10 +19,23 @@ import java.io.IOException;
 
 public class NatsNode implements RaaSNode {
     private static byte[] finalCompleteMessage = Data.newBuilder().setFinal("").build().toByteArray();
-    private Client nc;
 
-    public NatsNode(String address) throws IOException {
-        nc = new Client(address);
+    private Connection nc;
+
+    public NatsNode(Options options) throws IOException {
+        nc = new Connection(options);
+    }
+
+    @Override
+    public Observable<ServiceInfo> register(String serviceName, Service service) {
+        Subject<ServiceInfo> serviceInfoSubject = PublishSubject.create();
+        ServiceInfo info = new ServiceInfo();
+        Disposable subD = nc.subscribeMsg(serviceName, "service")
+                .flatMap(handshakeMsg -> onServiceConnected(service, handshakeMsg, serviceInfoSubject, info))
+                .subscribe(x -> {
+                }, serviceInfoSubject::onError);
+        return serviceInfoSubject
+                .doOnDispose(subD::dispose);
     }
 
     @Override
@@ -39,73 +53,80 @@ public class NatsNode implements RaaSNode {
         return null;
     }
 
-    @Override
-    public Observable<ServiceInfo> register(String serviceName, Service service) {
-        Subject<ServiceInfo> serviceInfoSubject = PublishSubject.create();
-        ServiceInfo info = new ServiceInfo();
-        Disposable subD = nc.subscribeMsg(serviceName)
-                .flatMapCompletable(msg -> {
-                    String clientPort = new String(msg.getBody());
-                    System.out.println("clientPort:"+clientPort);
-                    String servicePort = Utils.randomID();
-                    Msg replyMsg = new Msg(msg.getReplyTo(),servicePort.getBytes());
-                    Subject<byte[]> inputData = PublishSubject.create();
-                    Disposable d= nc.subscribeMsg(servicePort)
-                            .map(Msg::getBody)
-                            .map(Data::parseFrom)
-                            .takeUntil(x -> x.getTypeCase().getNumber() == 2)
-                            .doOnNext(data->{
-                                int type = data.getTypeCase().getNumber();
-                                if(type == 1)
-                                    inputData.onNext(data.getRaw().toByteArray());
-                                if(type == 2) {
-                                    String err = data.getFinal();
-                                    if(err==null || err.isEmpty())
-                                        inputData.onComplete();
-                                    else
-                                        inputData.onError(new Exception(err));
-                                }
-                            })
-                            .doOnDispose(inputData::onComplete)
-                            .subscribe();
-                    return service.onCall(new NatsContext(inputData))
-                            .doOnComplete(() -> outputComplete(clientPort))
-                            .doOnError(err -> outputError(clientPort, err))
-                            .doFinally(() -> System.out.println("call on final"))
-                            .map(x -> Data.newBuilder().setRaw(ByteString.copyFrom(x)).build())
-                            .map(x -> new Msg(clientPort, x.toByteArray()))
-                            .flatMapCompletable(out -> nc.publish(out))
-                            .doOnSubscribe(x -> {
-                                info.calledNum++;
-                                serviceInfoSubject.onNext(info);
-                            })
-                            .doOnComplete(() -> {
-                                info.completedNum++;
-                                serviceInfoSubject.onNext(info);
-                            })
-                            .doOnError(err -> {
-                                info.errorNum++;
-                                serviceInfoSubject.onNext(info);
-                            })
-                            .mergeWith(nc.publish(replyMsg))
-                            .doOnTerminate(d::dispose);
-                }).subscribe(() -> {
-                }, serviceInfoSubject::onError);
-        return serviceInfoSubject
-                .doOnDispose(subD::dispose);
+//
+//    @Override
+//    public Observable<byte[]> call(String serviceName, Observable<byte[]> outputData) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Observable<byte[]> subscribe(String subejct) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Completable publish(String subejct, byte[] data) {
+//        return null;
+//    }
+//
+
+    private Observable<byte[]> onServiceConnected(Service service, MSG handshakeMsg, Subject<ServiceInfo> serviceInfoSubject, ServiceInfo info) {
+        String clientPort = new String(handshakeMsg.getBody());
+        System.out.println("clientPort:" + clientPort);
+        String servicePort = Utils.randomID();
+        MSG replyMsg = new MSG(handshakeMsg.getReplyTo(), servicePort.getBytes());
+        Subject<byte[]> inputData = PublishSubject.create();
+        Disposable d = nc.subscribeMsg(servicePort)
+                .map(MSG::getBody)
+                .map(Data::parseFrom)
+                .takeUntil(x -> x.getTypeCase().getNumber() == 2)
+                .doOnNext(data -> {
+                    int type = data.getTypeCase().getNumber();
+                    if (type == 1)
+                        inputData.onNext(data.getRaw().toByteArray());
+                    if (type == 2) {
+                        String err = data.getFinal();
+                        if (err == null || err.isEmpty())
+                            inputData.onComplete();
+                        else
+                            inputData.onError(new Exception(err));
+                    }
+                })
+                .doOnDispose(inputData::onComplete)
+                .subscribe();
+        return service.onCall(new NatsContext(inputData))
+                .doOnNext(raw -> outputNext(clientPort, raw))
+                .doOnComplete(() -> outputComplete(clientPort))
+                .doOnError(err -> outputError(clientPort, err))
+                .doOnSubscribe(x -> {
+                    nc.publish(replyMsg);
+                    info.calledNum++;
+                    serviceInfoSubject.onNext(info);
+                })
+                .doOnComplete(() -> {
+                    info.completedNum++;
+                    serviceInfoSubject.onNext(info);
+                })
+                .doOnError(err -> {
+                    info.errorNum++;
+                    serviceInfoSubject.onNext(info);
+                })
+                .doFinally(() -> System.out.println("call on final"))
+                .doFinally(d::dispose);
+    }
+
+    private void outputNext(String clientPort, byte[] raw) {
+        Data data = Data.newBuilder().setRaw(ByteString.copyFrom(raw)).build();
+        nc.publish(new MSG(clientPort, data.toByteArray()));
     }
 
     private void outputComplete(String clientPort) {
-        nc.publish(new Msg(clientPort, finalCompleteMessage))
-                .subscribe(() -> {
-                }, err -> err.printStackTrace());
+        nc.publish(new MSG(clientPort, finalCompleteMessage));
     }
 
     private void outputError(String clientPort, Throwable t) {
         byte[] body = Data.newBuilder().setFinal(t.getMessage()).build().toByteArray();
-        nc.publish(new Msg(clientPort, body))
-                .subscribe(() -> {
-                }, err -> err.printStackTrace());
+        nc.publish(new MSG(clientPort, body));
     }
 
 }
---
+++ src/test/java/TestNatsSend.java
@@ -0,0 +1,45 @@
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNatsSend {
+
+    static long read;
+
+    public static void main(String[] args) {
+        try {
+
+            byte[] data = "hello".getBytes();
+            Connection nc = Nats.connect();
+
+
+            Observable.create(emitter -> {
+                Connection nc2 = Nats.connect();
+                nc2.subscribe("test", msg -> {
+                    read++;
+                });
+            }).subscribeOn(Schedulers.io()).subscribe();
+
+
+            Observable.create(emitter1 -> {
+                System.out.printf("publish on 1 :%s\n", Thread.currentThread().getName());
+                while (true) {
+                    nc.publish("test", data);
+                    //Thread.sleep(1000);
+                }
+            }).subscribeOn(Schedulers.io()).subscribe();
+
+            Observable.interval(1,TimeUnit.SECONDS)
+                    .subscribe(x -> System.out.printf("%d sec read: %d, ops: %d/s\n", x + 1, read, read / (x + 1)));
+
+//            Thread.sleep(Long.MAX_VALUE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
